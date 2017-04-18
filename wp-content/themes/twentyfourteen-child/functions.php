7914d280bc63cca0fc1de092a0c7b403da8a4ec0
paramireze <paramireze@gmail.com>
2017-04-18 07:47:44 -0500

    Tutorial 8 - include javascript files into child themes using enqueue
    https://www.youtube.com/watch?v=ocFvfCG5-Nc

--- wp-content/themes/twentyfourteen-child/functions.php
+++ wp-content/themes/twentyfourteen-child/functions.php
@@ -2,6 +2,8 @@
 
 function twentyfourteen_child_theme_scripts() {
 
-    wp_enqueue_script( 'extra js', get_template_directory_url() . '/js/extra.js' );
+    wp_enqueue_script( 'extra js', get_stylesheet_directory_uri() . '/js/extra.js' );
 
 }
+
+add_action( 'wp_enqueue_scripts', 'twentyfourteen_child_theme_scripts' );
