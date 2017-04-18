cbe4847d404d728e242148a80934eb939aabde12
paramireze <paramireze@gmail.com>
2017-04-18 13:22:45 -0500

    tutorial 12 - create a baritone theme
    https://www.youtube.com/watch?v=DspPgD4-EOY

---
+++ wp-content/themes/wordpress-tuts/inc/customizer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Wordpress Tuts Theme Customizer
+ *
+ * @package Wordpress_Tuts
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function wordpress_tuts_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+}
+add_action( 'customize_register', 'wordpress_tuts_customize_register' );
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function wordpress_tuts_customize_preview_js() {
+	wp_enqueue_script( 'wordpress_tuts_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+}
+add_action( 'customize_preview_init', 'wordpress_tuts_customize_preview_js' );
